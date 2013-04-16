831dc6b7226d6ac94e1a38993166ce0880d41fe5
Roddy Shuler <roddy@endlessm.com>
2013-04-16 09:36:14 -0700

    Current content of eos-games as a starting template for new eos-shell-apps [endlessm/eos-shell#63]

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+# Install dependencies
+DEPENDENCIES="devscripts debhelper"
+set +e 
+  dpkg -s $DEPENDENCIES &> /dev/null
+  has_dependencies=$?
+set -e
+
+if [[ $has_dependencies -ne 0 ]]; then
+  sudo apt-get install -y $DEPENDENCIES
+fi
+
+pushd `dirname $0`
+  
+  # Clean up old artifacts
+   set +e
+		rm -rf *.deb
+		rm -rf *.changes
+   set -e
+
+	pushd ../eos-build &> /dev/null
+      INSTALL_DIR=$(pwd)
+		export GNUPGHOME=${INSTALL_DIR}/gnupg
+	popd &> /dev/null
+
+  # Build package
+  debuild -k4EB55A92 -b
+  
+  # Move package to this directory and clean up
+   mv ../*games*.deb .
+   mv ../*games*.changes .
+   rm -f ../*.build
+popd
