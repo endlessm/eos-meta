8e8d243fe70d572b6d8ae1fb2ed122b6e2ed3aa8
Srdjan Grubor <sgnn7@sgnn7.org>
2013-10-11 15:17:34 -0500

    Added automatic way to increase version of endless-core

--- changeversion.sh
+++ changeversion.sh
@@ -1,4 +1,10 @@
 export DEBEMAIL=sgnn7@sgnn7.org
 export DEBFULLNAME="Srdjan Grubor"
 
-dch -i 
+OLD_VERSION=$(head -1 debian/changelog | awk '{print $2}' | sed -e 's:(\(.*[0-9\.]*\)):\1:g')
+echo "Old Version: $OLD_VERSION"
+
+NEW_VERSION=$(bc <<< "new_ver = $OLD_VERSION + 0.01; if (new_ver < 1) print 0; new_ver")
+echo "New Version: $NEW_VERSION"
+
+dch --force-distribution -D eos -v $NEW_VERSION
